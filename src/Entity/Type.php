a412b3d77a1944e921c621ac4335a349e8e032f8
Maciej <1412315+mymain@users.noreply.github.com>
2019-04-07 17:18:52 +0200

    Country & Type JMS serializations setup

--- src/Entity/Type.php
+++ src/Entity/Type.php
@@ -2,14 +2,16 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TypeRepository")
  * @UniqueEntity("name", message="Type name is already in use.")
+ * @JMS\ExclusionPolicy("all")
  */
 class Type
 {
@@ -17,11 +19,13 @@ class Type
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @JMS\Expose()
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=191, unique=true)
+     * @JMS\Expose()
      */
     private $name;
 
